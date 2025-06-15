8e525b941db9876b7d09a0429d8f9cd4e0ffff93
Fiyuudump <fiantyogalih@gmail.com>
2025-06-15 16:30:37 +0700

    feat: add CRUD system for barang and jenis at admin page

--- blog/profil/admin.py
+++ blog/profil/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Barang, Jenis
 
 # Register your models here.
+class BarangAdmin(admin.ModelAdmin):
+    list_display=['kdbrg', 'nama', 'stok', 'harga', 'link_gambar', 'waktu_posting', 'jenis_id']
+    search_fields=['kdbrg', 'nama', 'jenis_id__nama']
+    list_filter=('nama', 'jenis_id',)
+    list_per_page=5
+
+admin.site.register(Barang, BarangAdmin)
+admin.site.register(Jenis)
--- blog/profil/models.py
+++ blog/profil/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+class Jenis(models.Model):
+    nama = models.CharField(max_length=20)
+    deskripsi = models.TextField()
+
+    def _str_(self):
+        return self.nama
+
 # Create your models here.
+class Barang(models.Model):
+    kdbrg = models.CharField(max_length=8)
+    nama = models.CharField(max_length=75)
+    stok = models.IntegerField()
+    harga = models.BigIntegerField()
+    link_gambar = models.CharField(max_length=150, blank=True)
+    waktu_posting = models.DateTimeField(auto_now_add=True)
+    jenis_id = models.ForeignKey(Jenis, on_delete=models.CASCADE, null=True)
+
+    def _str_(self):
+        return self.nama
