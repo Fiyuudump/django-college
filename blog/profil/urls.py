fa5fb0e1aba5be247d36cf3bdbf7af4666107c14
Fiyuudump <fiantyogalih@gmail.com>
2025-06-20 17:03:22 +0700

    feat: setup root path of home

--- blog/profil/urls.py
+++ blog/profil/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from profil import views
 
 urlpatterns = [
-    path('omah/', views.home),
-    path('about/', views.about),
+    path('', views.home),
+    # path('about/', views.about),
 ]
