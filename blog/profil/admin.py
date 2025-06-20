f939717f8f4cf5af778d49eb3cb4308f4ce6bb7f
Fiyuudump <fiantyogalih@gmail.com>
2025-06-20 17:06:50 +0700

    BREAKING CHANGE: setup new theme of admin dashboard

--- blog/profil/admin.py
+++ blog/profil/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
-from .models import Barang, Jenis
+from . models import Barang, Jenis, About, Project
 
-# Register your models here.
-class BarangAdmin(admin.ModelAdmin):
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import User, Group
+
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+from unfold.admin import ModelAdmin
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    # Forms loaded from `unfold.forms`
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
+
+
+class BarangAdmin(ModelAdmin):
     list_display=['kdbrg', 'nama', 'stok', 'harga', 'link_gambar', 'waktu_posting', 'jenis_id']
     search_fields=['kdbrg', 'nama', 'jenis_id__nama']
     list_filter=('nama', 'jenis_id',)
@@ -10,3 +32,6 @@ class BarangAdmin(admin.ModelAdmin):
 
 admin.site.register(Barang, BarangAdmin)
 admin.site.register(Jenis)
+admin.site.register(About)
+admin.site.register(Project)
+
